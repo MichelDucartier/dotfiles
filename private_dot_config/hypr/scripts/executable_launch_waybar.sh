deab8f5b090e4bccc844da863b8e749c68d63ef3
MichelDucartier <m.zhang2490@gmail.com>
2025-06-14 19:49:59 +0200

    Update hypr config

--- private_dot_config/hypr/scripts/executable_launch_waybar.sh
+++ private_dot_config/hypr/scripts/executable_launch_waybar.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-monitor=$(hyprctl monitors -j | jq "length")
-monitor="$((monitor - 1))"
+# monitor=$(hyprctl monitors -j | jq "length")
+# monitor="$((monitor - 1))"
+monitor=0
 eww open bar --screen $monitor
